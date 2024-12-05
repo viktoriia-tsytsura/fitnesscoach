9b93c8a9fdc9d4857d43ab65b929298268065e95
Viktoriia Sholom <viktoriiasholom@Viktoriias-MacBook-Pro.local>
2024-12-05 23:00:29 +0100

    fix tariffs wide

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,14 +1,22 @@
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector('[data-menu-open]'),
-    closeMenuBtn: document.querySelector('[data-menu-close]'),
-    menu: document.querySelector('[data-menu]'),
+    openMenuBtn: document.querySelector("[data-menu-open]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    menu: document.querySelector("[data-menu]"),
+    navigation: document.querySelector("[data-navigation]"),
   };
 
-  refs.openMenuBtn.addEventListener('click', toggleModal);
-  refs.closeMenuBtn.addEventListener('click', toggleModal);
+  refs.openMenuBtn.addEventListener("click", toggleModal);
+  refs.closeMenuBtn.addEventListener("click", toggleModal);
+  refs.navigation.addEventListener("click", handleNavigationClick);
 
   function toggleModal() {
-    refs.menu.classList.toggle('is-open');
+    refs.menu.classList.toggle("is-open");
+  }
+
+  function handleNavigationClick(e) {
+    if (e.target.tagName === "A" || e.target.tagName === "BUTTON") {
+      toggleModal();
+    }
   }
 })();
